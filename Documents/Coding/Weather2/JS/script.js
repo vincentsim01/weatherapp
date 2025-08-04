cdcf364aa6bb0022c1d60aaa57b8028fe738dfe0
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-05 00:53:45 +0800

    3 box

--- Documents/Coding/Weather2/JS/script.js
+++ Documents/Coding/Weather2/JS/script.js
@@ -8,10 +8,7 @@ async function getweatherfunction(){
     const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citytextinput.value}&appid=286ad2f78fee5038e5f4c39b2c60a946`)
     const data = await weatherdata.json();
     textchangecontainer.classList.remove('hidden');
-
-
     changetext.innerHTML = `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
-
 }
 
 function closetextchangecontainer(){
@@ -19,3 +16,31 @@ function closetextchangecontainer(){
 }
 
 getweatherbutton.addEventListener('click',getweatherfunction);
+
+async function setthreecityone(){
+    const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=286ad2f78fee5038e5f4c39b2c60a946`)
+    const data = await weatherdata.json();
+    document.getElementById('threecityone').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+}
+
+setTimeout(setthreecityone(),0);  
+
+async function setthreecitytwo(){
+    const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Copenhagen&appid=286ad2f78fee5038e5f4c39b2c60a946`)
+    const data = await weatherdata.json();
+    document.getElementById('threecitytwo').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+}
+
+setTimeout(setthreecitytwo(),0); 
+
+
+async function setthreecitythree(){
+    const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=286ad2f78fee5038e5f4c39b2c60a946`)
+    const data = await weatherdata.json();
+    document.getElementById('threecitythree').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+}
+
+setTimeout(setthreecitythree(),0);  
+
+
+
