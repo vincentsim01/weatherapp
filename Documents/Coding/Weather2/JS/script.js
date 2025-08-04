2a5d2041e986326146a31010c80596ac0a34825f
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-04 21:02:32 +0800

    insert text

--- Documents/Coding/Weather2/JS/script.js
+++ Documents/Coding/Weather2/JS/script.js
@@ -1,8 +1,10 @@
 const getweatherbutton = document.getElementById('getweatherbutton');
-const changetext = document.getElementById('changetext')
+const changetext = document.getElementById('changetext');
+const citytextinput = document.getElementById('citytextinput').value;
 
 async function getweatherfunction(){
-    const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=51.48&lon=-0.14&appid=286ad2f78fee5038e5f4c39b2c60a946`)
+    const citytextinput = document.getElementById('citytextinput');
+    const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citytextinput.value}   &appid=286ad2f78fee5038e5f4c39b2c60a946`)
     const data = await weatherdata.json();
 
     changetext.innerHTML = `The Temperature in ${data.name}, ${data.sys.country} is ${data.main.temp} F`;
