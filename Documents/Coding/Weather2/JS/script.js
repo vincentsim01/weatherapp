ac99432e9e889559cdfb8306c795491965cd5307
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-08-05 11:01:15 +0800

    object

--- Documents/Coding/Weather2/JS/script.js
+++ Documents/Coding/Weather2/JS/script.js
@@ -20,7 +20,7 @@ getweatherbutton.addEventListener('click',getweatherfunction);
 async function setthreecityone(){
     const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=286ad2f78fee5038e5f4c39b2c60a946`)
     const data = await weatherdata.json();
-    document.getElementById('threecityone').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+    document.getElementById('threecityone').innerHTML= `<img src="./asset/indonesia.png" class="w-[20px] rounded-full border border-white shadow-3xl mx-auto"></img><br>${data.sys.country} <br> <b>${data.name}</b> <br> ${data.main.temp} F`;
 }
 
 setTimeout(setthreecityone(),0);  
@@ -28,7 +28,7 @@ setTimeout(setthreecityone(),0);
 async function setthreecitytwo(){
     const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Copenhagen&appid=286ad2f78fee5038e5f4c39b2c60a946`)
     const data = await weatherdata.json();
-    document.getElementById('threecitytwo').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+    document.getElementById('threecitytwo').innerHTML= `<img src="./asset/denmark.png" class="w-[20px] rounded-full border border-white shadow-3xl mx-auto"></img><br>${data.sys.country} <br> <b>${data.name}</b> <br> ${data.main.temp} F`;
 }
 
 setTimeout(setthreecitytwo(),0); 
@@ -37,10 +37,15 @@ setTimeout(setthreecitytwo(),0);
 async function setthreecitythree(){
     const weatherdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=286ad2f78fee5038e5f4c39b2c60a946`)
     const data = await weatherdata.json();
-    document.getElementById('threecitythree').innerHTML= `${data.sys.country} <br> ${data.name} <br> ${data.main.temp} F`;
+    document.getElementById('threecitythree').innerHTML= `<img src="./asset/netherlands.png" class="w-[20px] rounded-full border border-white shadow-3xl mx-auto"></img><br>${data.sys.country} <br> <b>${data.name}</b> <br> ${data.main.temp} F`;
 }
 
 setTimeout(setthreecitythree(),0);  
 
+const bannerbackgroundimage = [kinderdijk.png, jiuzhaigou.jpg, grandcanyon.jpg]
 
+function changebannerbackground(){
+    const mainbanner = document.getElementById('mainbanner');
+    
+}
 
